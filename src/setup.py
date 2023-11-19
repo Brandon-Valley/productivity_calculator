9d3375ee6b1bfcd5c70bfe8a485f7a4e7de495dd
Brandon Valley <bavvh8@mst.edu>
2023-11-18 19:47:27 -0600

    _get_input_file_tups_from_data_file_paths() works

--- src/setup.py
+++ src/setup.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
-
 # Usage:
 #  - To just run a quick test for the built exe, run:  `python setup.py build`
 #  - To build the full .msi (which takes ~10 sec longer) run:  `python setup.py bdist_msi`
 
-# https://stackoverflow.com/questions/57339138/msi-created-in-cx-freeze-does-not-install-program
-# msiexec -i "Productivity Calculator-0.0.3-win64.msi" -l*vx log.txt
+# Future Improvements:
+#  - Add option to prompt user to open program after install
+#    - https://cx-freeze-users.narkive.com/xThwhu4x/cx-freeze-bdist-msi-install-script-option
 
-
+from typing import List, Tuple
 from cx_Freeze import setup, Executable
 from pathlib import Path
 import sys
 import cfg
+from os.path import relpath
 
 SCRIPT_PARENT_DIR_PATH = Path("__file__").parent
-
 
 # base="Win32GUI" should be used only for Windows GUI app
 BASE = "Win32GUI" if sys.platform == "win32" else None
@@ -29,6 +29,13 @@ PRODUCT_DESCRIPTION = 'Uses exports from QuickEMR & opentimeclock.com to calcula
 TOP_LEVEL_PY_FILE_PATH = SCRIPT_PARENT_DIR_PATH  / "gui.py"
 
 EXE_ICON_ICO_PATH = SCRIPT_PARENT_DIR_PATH / "imgs" / "icon.ico"
+GUI_ICON_PNG_PATH = SCRIPT_PARENT_DIR_PATH / "imgs" / "icon.png"
+
+# Paths to non-python data/config files that need to be copied to the build dir so they will be accessible by the python
+# script files after freeze. - icon image paths used for the GUI for example
+DATA_FILE_PATHS = [
+    GUI_ICON_PNG_PATH
+]
 
 ADD_DESKTOP_SHORTCUT_FROM_MSI = True
 ADD_STARTUP_SHORTCUT_FROM_MSI = False
@@ -37,13 +44,23 @@ ADD_START_MENU_SHORTCUT_FROM_MSI = False
 SHOW_CMD = False # Useful for testing
 
 
-ICON_STR_PATH = "imgs//icon.ico" # FIX rename to rel pat?
 ICON_PNG_STR_PATH = "imgs//icon.png" # FIX rename to rel pat?
 
 # Derived
 EXE_FILE_NAME = cfg.PRODUCT_NAME + ".exe"
 
 
+def _get_input_file_tups_from_data_file_paths(data_file_paths: List[Path]) -> List[Tuple[str, str]]:
+    """
+    (This example assumes this file exists at c/p/myapp/src/setup.py)
+    Input: [Path("c/p/myapp/src/imgs/gui_icon.png"), Path("c/p/myapp/src/config/some_file.txt")]
+    Output: 
+    [
+        ("c/p/myapp/src/imgs/gui_icon.png", "imgs/gui_icon.png"),
+        ("c/p/myapp/src/config/some_file.txt", "config/some_file.txt"),
+    ]
+    """
+    return [(data_file_path.as_posix(), relpath(data_file_path.as_posix(), SCRIPT_PARENT_DIR_PATH.as_posix())) for data_file_path in data_file_paths]
 
 
 def _get_shortcut_table():
@@ -85,6 +102,7 @@ def _get_shortcut_table():
             )
         )
     if ADD_START_MENU_SHORTCUT_FROM_MSI:
+        # Possible solution: https://github.com/marcelotduarte/cx_Freeze/issues/48
         raise NotImplementedError("Pinning the shortcut to start from msi is not yet implemented")
 
     return shortcut_table
@@ -121,9 +139,11 @@ setup(
         "build_exe": {
             # "includes": BasicPackages,
             # "excludes": [i for i in AllPackage() if notFound(BasicPackages,i)],
-            "include_files": [
-                (ICON_PNG_STR_PATH, "imgs/icon.png") # Include .png icon for GUI to use
-                ],
+            "include_files": _get_input_file_tups_from_data_file_paths(DATA_FILE_PATHS)
+            
+            # [
+            #     (ICON_PNG_STR_PATH, "imgs/icon.png") # Include .png icon for GUI to use
+            #     ],
             # "zip_include_packages": ["encodings"] ##
         },
 
