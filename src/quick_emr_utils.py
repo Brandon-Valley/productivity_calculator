6a925c8b106c2b7fc37963cfcad7c6589e15f0ae
Brandon Valley <bavvh8@mst.edu>
2024-07-21 21:01:30 -0500

    format

--- src/quick_emr_utils.py
+++ src/quick_emr_utils.py
@@ -1,5 +1,3 @@
-
-
 from datetime import datetime
 from pathlib import Path
 import logging
@@ -23,7 +21,10 @@ ROW_TYPE_FACILITY_NAME = "FACILITY_NAME"
 ROW_TYPE_PROVIDER_NAME = "PROVIDER_NAME"
 ROW_TYPE_DATE_DATA = "DATE_DATA"
 
-def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_productivity_csv_export(in_csv_path: Path, facility_names) -> dict:
+
+def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_productivity_csv_export(
+    in_csv_path: Path, facility_names
+) -> dict:
     """
     Example input csv:
 
@@ -48,17 +49,18 @@ def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_producti
         ...
     }
     """
+
     def _validate_provider_productivity_csv_format(row_dicts: List[dict]) -> None:
         try:
             first_row_headers = list(row_dicts[0].keys())
         except IndexError:
             raise ValueError(f"Format Error: {in_csv_path=} - CSV is empty")
-        
+
         assert (h in first_row_headers for h in PROVIDER_PRODUCTIVITY_HEADER_NAMES), (
             f"Format Error: {in_csv_path=} - CSV is missing one or more expected headers from "
             f"{PROVIDER_PRODUCTIVITY_HEADER_NAMES}="
         )
-        
+
         assert len(first_row_headers) == len(PROVIDER_PRODUCTIVITY_HEADER_NAMES), (
             f"Format Error: {in_csv_path=} - CSV has a different number of headers than expected: {first_row_headers=} "
             f"vs. {PROVIDER_PRODUCTIVITY_HEADER_NAMES=}"
@@ -66,14 +68,13 @@ def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_producti
 
         for row_dict in row_dicts:
             for key, value in row_dict.items():
-                assert (value != "" and value != None), f"Format Error: {in_csv_path=} - CSV has blank value at {key=}"
+                assert value != "" and value != None, f"Format Error: {in_csv_path=} - CSV has blank value at {key=}"
 
     def _get_date_datetime_from_date_str(date_str: str) -> datetime:
         """Example input: <a target="note_226711" href="/TP001V8/note?note_id=226711&amp;tab=charge">07/11/2024</a>"""
         date_str = date_str.split(">")[1].split("<")[0]
         date_datetime = datetime.strptime(date_str, "%m/%d/%Y")
         return date_datetime
-    
 
     row_dicts = file_io_utils.read_csv_as_row_dicts(in_csv_path)
 
@@ -88,7 +89,8 @@ def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_producti
 
         if facility_name not in facility_names:
             raise NotImplementedError(
-                f"Facility name '{facility_name}' not in {facility_names=} - Have not needed to deal with this yet.")
+                f"Facility name '{facility_name}' not in {facility_names=} - Have not needed to deal with this yet."
+            )
 
         date_datetime = _get_date_datetime_from_date_str(date_str)
 
@@ -101,7 +103,7 @@ def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_producti
         productivity_data_dicts_by_date_by_provider_name[provider_name][date_datetime].append(row_dict)
 
         # Remove 'DOS', 'Facility', and 'Provider Details' from productivity_data_dicts_by_date_by_provider_name
-        #   - Only doing this b/c these fields were not needed in the previous version of this function (before the 
+        #   - Only doing this b/c these fields were not needed in the previous version of this function (before the
         #     Quick EMR update changed the csv format)
         for key in ["DOS", "Facility", "Provider Details"]:
             row_dict.pop(key)
@@ -109,16 +111,15 @@ def _get_productivity_data_dicts_by_date_by_provider_name_from_provider_producti
     return productivity_data_dicts_by_date_by_provider_name
 
 
-
-
 def get_total_units_by_date_by_provider_name_from_provider_productivity_csv_export(in_csv_path: Path, facility_names):
     logging.info("in get_total_units_by_date_by_provider_name_from_provider_productivity_csv_export")
 
     total_units_by_date_by_provider_name = {}
-    productivity_data_dicts_by_date_by_provider_name = _get_productivity_data_dicts_by_date_by_provider_name_from_provider_productivity_csv_export(
-        in_csv_path, facility_names)
-    
-    # pprint(productivity_data_dicts_by_date_by_provider_name)#TMP
+    productivity_data_dicts_by_date_by_provider_name = (
+        _get_productivity_data_dicts_by_date_by_provider_name_from_provider_productivity_csv_export(
+            in_csv_path, facility_names
+        )
+    )
 
     for provider_name, productivity_data_dicts_by_date in productivity_data_dicts_by_date_by_provider_name.items():
         total_units_by_date_by_provider_name[provider_name] = {}
